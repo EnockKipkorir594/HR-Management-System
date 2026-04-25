2242660567cdd9f2de23b51ba6694dd4fe1b773b
Enock <kipkorirenock594@gmail.com>
2026-04-25 12:28:37 +0300

    Updated the config.py file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask 
 from app.extensions import db
-from config import Development
+from config import config
 
-def create_app(config_class=Development):
+def create_app(config_class=None):
     
     app = Flask(__name__ , instance_relative_config=True)
     #Load settings from config class
-    app.config.from_object(config_class)
+    app.config.from_object(config)
     #loading database configurations
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 '''secret_ key --> generate random hex characters
 run --> python3 -c 'import secrets; print(secrets.token_hex())'
 '''
-class config_class(object):
+class Config(object):
     #for production 
     TESTING = False 
     #Database server for producton
@@ -12,16 +12,26 @@ class config_class(object):
         return f"mysql://user@{self.DB_SERVER}/foo"
     
 #Production environment
-class Production(config_class):
+class Production(Config):
     DEBUG = False
     DB_SERVER = '192.168.1.32'
     
 #Development environmet where debugging is true   
-class Development(config_class):
+class Development(Config):
     DEBUG = True
     DB_SERVER ='localhost'
     
 #Testing environment    
-class Testing(config_class):
+class Testing(Config):
     DB_SERVER = 'localhost'
     DATABASE_URI = 'sqlite:///:memory:'
+    
+    
+    
+config = {
+    "development": Development,
+    "testing": Testing,
+    "production": Production,
+    "default": Development
+    
+}
